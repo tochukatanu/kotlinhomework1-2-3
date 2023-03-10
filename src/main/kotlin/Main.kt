d8abed915724a5af332ad3275afa08a06d95f815
Anton Trifonov <royfokker.moto@gmail.com>
2023-03-10 17:33:36 +0400

    changing conditional operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,23 +2,28 @@ package ru.netology
 
 fun main() {
 
-    var purchaseSum = 15000.0 // var - условно, тк в теории меняется при ОС от пользовател
+    var purchaseSum = 900 // var - условно, тк в теории меняется при ОС от пользователя
     val fixedDiscount = 100.0
     val discount = 0.05
     val monthlyDiscountRate = 0.01
     var total = 0.0
-    var isRegularCustomer = false // при изменении на true применяется скидка постоянного покупателя
+    val isRegularCustomer = false // при изменении на true применяется скидка постоянного покупателя
     if (purchaseSum < 0) {
         println("Сумма покупки не может быть отрицательной")
     }
-    total = if (0 < purchaseSum && purchaseSum < 1000) {
-        purchaseSum
-    } else if (1000 < purchaseSum && purchaseSum < 10_000) {
-        purchaseSum - fixedDiscount
-    } else {
-        purchaseSum - purchaseSum * discount
+//    total = if (0 < purchaseSum && purchaseSum < 1000) {
+//        purchaseSum
+//    } else if (1000 < purchaseSum && purchaseSum < 10_000) {
+//        purchaseSum - fixedDiscount
+//    } else {
+//        purchaseSum - purchaseSum * discount
+//    }
+    total = when (purchaseSum) {
+        in 0..1000 -> purchaseSum.toDouble()
+        in 1001..10000 -> purchaseSum - fixedDiscount
+        else -> purchaseSum - purchaseSum * discount
     }
-    if (isRegularCustomer == true) {
+    if (isRegularCustomer) {
         total -= total * monthlyDiscountRate
     }
     println("Сумма вашей покупки $total")
